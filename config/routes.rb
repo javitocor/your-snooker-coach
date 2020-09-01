a166316b541532035f5b353d14966994e92a5b72
javitocor <javitocor@gmail.com>
2020-09-01 18:44:47 +0200

    create controller, routes and static views. set up database

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'welcome/home'
+  get 'welcome/app'
+  get 'welcome/home'
+  get '/app', to: 'welcome#app', as: 'app'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'welcome#home'
 end
