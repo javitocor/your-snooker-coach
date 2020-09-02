7903ed6bfc8d967cd50d84ac979150279b40ac1f
javitocor <javitocor@gmail.com>
2020-09-02 05:59:09 +0200

    add validation for training dates

--- app/models/training.rb
+++ app/models/training.rb
@@ -2,7 +2,13 @@ class Training < ApplicationRecord
   belongs_to :user
   belongs_to :player
 
+  validate :valid_date
+
   scope :ordered_by_most_recent, -> { order(created_at: :desc) }
 
   validates_presence_of :date, :location, :player_id
+
+  def valid_date
+    errors.add(:date, "Can't book in the past") if date.present? && date < Date.current
+  end
 end
