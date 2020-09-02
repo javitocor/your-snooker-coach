75ea188c3c133001ba83e0f55b4b97d960c98192
javitocor <javitocor@gmail.com>
2020-09-02 06:15:19 +0200

    add column to players table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_02_033030) do
+ActiveRecord::Schema.define(version: 2020_09_02_041352) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2020_09_02_033030) do
     t.datetime "updated_at", null: false
     t.string "image"
     t.integer "ranking"
+    t.string "rate"
   end
 
   create_table "trainings", force: :cascade do |t|
