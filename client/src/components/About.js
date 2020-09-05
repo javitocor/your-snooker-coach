87e38a694a16addd2ac6c649d7de2ebd318fec5e
javitocor <56809963+javitocor@users.noreply.github.com>
2020-09-05 10:58:41 +0200

    implement about page

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,7 +1,39 @@
 import React from 'react';
 
 const About = () => (
-  
+  <main class="col-md-10 col-sm-12 p-3">
+    <div class="jumbotron">
+      <div class="container">
+        <h1 class="display-3">Xin Chào, Snooker Lovers</h1>
+        <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.</p>
+        <p><a class="btn btn-success btn-lg" href="#" role="button">Learn more »</a></p>
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="row">
+        <div class="col-md-4">
+          <h2>Who We Are</h2>
+          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+          <p><a class="btn btn-success" href="#" role="button">View details »</a></p>
+        </div>
+        <div class="col-md-4">
+          <h2>What We Do</h2>
+          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+          <p><a class="btn btn-success" href="#" role="button">View details »</a></p>
+        </div>
+        <div class="col-md-4">
+          <h2>Contact Us</h2>
+          <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+          <p><a class="btn btn-success" href="#" role="button">View details »</a></p>
+        </div>
+      </div>
+
+      <hr />
+
+    </div>
+
+  </main>
 );
 
 export default About;
