de0b5a112e96f1e5c920f158cf8d241f8b55b138
javitocor <56809963+javitocor@users.noreply.github.com>
2020-09-05 10:56:38 +0200

    Created navbar and about components

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,16 +3,22 @@ import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import Landing from '../containers/Landing';
 import ProfileCoach from '../containers/ProfileCoach';
 import ProfileUser from '../containers/ProfileUser';
+import About from './About';
+import NavBar from './NavBar';
 
 
 const App = () => (
   <Router>
     <Switch>
       <>
-        <div className="app">
-          <Route exact path="/" component={Landing} />
-          <Route exact path="/coach/:id" component={ProfileCoach} />
-          <Route exact path="/user/trainings" component={ProfileUser} />
+        <div class=" p-3 mt-3 mb-3 bg-dark">
+          <div class="row d-sm-flex flex-sm-column d-md-flex flex-md-row">
+            <Route path="/" component={NavBar} />
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/coach/:id" component={ProfileCoach} />
+            <Route exact path="/user/trainings" component={ProfileUser} />
+            <Route exact path="/about" component={About} />
+          </div>
         </div>
       </>
     </Switch>
