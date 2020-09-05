c13fe28c7c906cf282669a78b6acbee2303b4a28
javitocor <56809963+javitocor@users.noreply.github.com>
2020-09-05 11:28:04 +0200

    create carousel component

--- client/src/containers/Landing.js
+++ client/src/containers/Landing.js
@@ -4,6 +4,7 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { Link } from 'react-router-dom';
 import { AllPlayersCall } from '../helpers/APIcalls';
+import '../style/Landing.css';
 
 class Landing extends React.Component {
   componentDidMount() {
@@ -15,29 +16,20 @@ class Landing extends React.Component {
   render() { 
     const { coach } = this.props;
     const { playersList } = coach;
-    console.log(playersList);
     return ( 
-      <div>
-        <div>
-          {playersList.map(player => (
-            <ul>
-              <li>{player.name}</li>
-              <li><Link
-              key={player.id}
-              to={{
-                pathname: `/coach/${player.id}`,
-                state: {
-                  playerId: player.id,
-                },
-              }}
-              className="btn btn-secondary"
-              >
-                Go to Coach Profile
-              </Link></li>
-            </ul>
-          ))}
+      <div className="col-md-10 col-sm-12 p-3">
+        <div className="d-flex align-items-centerjustify-content-start text-success ml-2">
+          <h3><u>HOME</u></h3>
         </div>
-      </div>     
+        <div className="text-center ">
+          <h3 className="text-white bg-success w-50 m-auto rounded p-2 border border-dark d-flex justify-content-around">
+            Select Your Coach<small className="mt-2"><i className="fas fa-chevron-down"></i></small></h3>
+          <span className="tex-mute text-white-50">Select over top-className professional players</span>
+        </div>
+        <div className="jumbotron bg-success p-4 mt-5">
+          <Carousel key={playersList} playerslist={playersList} />
+        </div>
+      </div>
     );
   }
 }
@@ -64,3 +56,19 @@ const mapDispatchToProps = dispatch => bindActionCreators({
 }, dispatch);
 
 export default connect(mapStateToProps, mapDispatchToProps)(Landing);
+
+
+
+
+<Link
+              key={player.id}
+              to={{
+                pathname: `/coach/${player.id}`,
+                state: {
+                  playerId: player.id,
+                },
+              }}
+              classNameName="btn btn-secondary"
+              >
+                Go to Coach Profile
+              </Link>   
