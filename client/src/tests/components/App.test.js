b789f30e36ad2f04e89fadef8b35b69090d6a4bf
javitocor <56809963+javitocor@users.noreply.github.com>
2020-09-07 19:32:25 +0200

    create apicalls test

--- client/src/tests/components/App.test.js
+++ client/src/tests/components/App.test.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import { render, screen } from '../customRender';
 import App from '../../components/App';
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+
+const middlewares = [thunk];
+const mockStore = configureMockStore(middlewares);
 
 describe('<App />', () => {
   it('Renders <App /> component correctly', () => {
