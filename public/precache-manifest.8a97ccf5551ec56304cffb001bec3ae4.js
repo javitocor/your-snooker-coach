d5e707675279a080e3297abd24cb621d1e9d6ca9
javitocor <javitocor@gmail.com>
2020-09-08 15:05:38 +0200

    precompile assets

---
+++ public/precache-manifest.8a97ccf5551ec56304cffb001bec3ae4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "136c063f0cfed20dcf7ed7d108703f0b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "986f29d4548436403536",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "551ac74c94ce540af562",
+    "url": "/static/css/main.87541a83.chunk.css"
+  },
+  {
+    "revision": "986f29d4548436403536",
+    "url": "/static/js/2.c4c04bba.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.c4c04bba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "551ac74c94ce540af562",
+    "url": "/static/js/main.274c229b.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
