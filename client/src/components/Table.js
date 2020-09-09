97be2acf4adda56658821c8528a0c9ccddf7095d
javitocor <56809963+javitocor@users.noreply.github.com>
2020-09-09 15:25:00 +0200

    make the coachprofile table responsive

--- client/src/components/Table.js
+++ client/src/components/Table.js
@@ -5,30 +5,30 @@ import '../style/Table.css';
 const Table = props => {
   const { player } = props;
   return (
-    <div className="table-responsive my-sm-2 mt-3 bg-light border border-dark">
+    <div className="my-sm-2 mt-3 bg-light border border-dark">
       <div className=" text-right pr-2 black m-0 pt-1">
         <h4 className="text-right mb-0 mt-3 text-success">Coach Info</h4>
         <small className="text-right text-muted">Everything you need to know</small>
       </div>
       <table className="table table-hover table-bordered table-striped mb-0">
         <tbody>
-          <tr className="">
+          <tr className="as">
             <th className="text-center bg-success"><i className="far fa-clock"></i></th>
             <th className="d-flex justify-content-between align-items-center"><p className="m-0 text-dark"> D.O.B:</p> <p className="m-0 text-dark">{player.born}</p></th>
           </tr>
-          <tr className="">
+          <tr className="as">
             <th className="text-center bg-success"><i className="fas fa-globe-europe"></i></th>
             <th className="d-flex justify-content-between align-items-center"><p className="m-0 text-dark"> Nationality:</p> <p className="m-0 text-dark">{player.nationality}</p></th>
           </tr>
-          <tr className="">
+          <tr className="as">
             <th className="text-center bg-success"><i className="far fa-star"></i></th>
             <th className="d-flex justify-content-between align-items-center"><p className="m-0 text-dark"> Turned Pro:</p> <p className="m-0 text-dark">{player.firstseason}</p></th>
           </tr>
-          <tr className="">
+          <tr className="as">
             <th className="text-center bg-success"><i className="fas fa-trophy"></i></th>
             <th className="d-flex justify-content-between align-items-center"><p className="m-0 text-dark"> Ranking:</p> <p className="m-0 text-dark">{player.ranking}</p></th>
           </tr>
-          <tr className="">
+          <tr className="as">
             <th className="text-center bg-success"><i className="fas fa-dollar-sign"></i></th>
             <th className="d-flex justify-content-between align-items-center"><p className="m-0 text-dark">   Rate:</p> <p className="m-0 text-dark">{player.rate}</p></th>
           </tr>
