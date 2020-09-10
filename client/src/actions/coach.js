390d9b1e1ed0592fa1e274f9007a87988ff7514e
javitocor <56809963+javitocor@users.noreply.github.com>
2020-09-10 13:03:18 +0200

    create and implement delete and update actions for players

--- client/src/actions/coach.js
+++ client/src/actions/coach.js
@@ -1,4 +1,8 @@
-import { GET_ALL_PLAYERS, GET_ALL_PLAYERS_ERROR, GET_ALL_PLAYERS_PENDING, GET_PLAYER, GET_PLAYER_ERROR, GET_PLAYER_PENDING } from '../helpers/constants';
+import { GET_ALL_PLAYERS, GET_ALL_PLAYERS_ERROR, GET_ALL_PLAYERS_PENDING, 
+  GET_PLAYER, GET_PLAYER_ERROR, GET_PLAYER_PENDING,
+  DELETE_PLAYER, DELETE_PLAYER_PENDING, DELETE_PLAYER_ERROR,
+  UPDATE_PLAYER, UPDATE_PLAYER_PENDING, UPDATE_PLAYER_ERROR
+ } from '../helpers/constants';
 
 export const getAllPlayers = (playersList) => ({
   type: GET_ALL_PLAYERS,
@@ -26,4 +30,32 @@ export const getPlayerPending = () => ({
 export const getPlayerError = (error) => ({
   type: GET_PLAYER_ERROR,
   error
+});
+
+export const deletePlayer = (player) => ({
+  type: DELETE_PLAYER,
+  player,
+});
+
+export const deletePlayerPending = () => ({
+  type: DELETE_PLAYER_PENDING,
+});
+
+export const deletePlayerError = (error) => ({
+  type: DELETE_PLAYER_ERROR,
+  error
+});
+
+export const updatePlayer = (player) => ({
+  type: UPDATE_PLAYER,
+  player
+});
+
+export const updatePlayerPending = () => ({
+  type: UPDATE_PLAYER_PENDING,
+});
+
+export const updatePlayerError = (error) => ({
+  type: UPDATE_PLAYER_ERROR,
+  error,
 });
