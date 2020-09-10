b4618dca0fc3f4bd7cb306f8294c0bf01a0e3958
javitocor <56809963+javitocor@users.noreply.github.com>
2020-09-10 14:53:15 +0200

    implement players update and delete actions in the reducer

--- client/src/reducers/coach.js
+++ client/src/reducers/coach.js
@@ -1,4 +1,8 @@
-import { initialStatePlayers, GET_ALL_PLAYERS, GET_ALL_PLAYERS_ERROR, GET_ALL_PLAYERS_PENDING, GET_PLAYER, GET_PLAYER_ERROR, GET_PLAYER_PENDING } from '../helpers/constants';
+import { GET_ALL_PLAYERS, GET_ALL_PLAYERS_ERROR, GET_ALL_PLAYERS_PENDING, 
+  GET_PLAYER, GET_PLAYER_ERROR, GET_PLAYER_PENDING,
+  DELETE_PLAYER, DELETE_PLAYER_PENDING, DELETE_PLAYER_ERROR,
+  UPDATE_PLAYER, UPDATE_PLAYER_PENDING, UPDATE_PLAYER_ERROR
+ } from '../helpers/constants';
 
 const coachReducer = (state = initialStatePlayers, action) => {
   switch (action.type) {
@@ -31,6 +35,39 @@ const coachReducer = (state = initialStatePlayers, action) => {
         pending: true,
       };
     case GET_PLAYER_ERROR:
+      return {
+        ...state,
+        pending: false,
+        error: action.error,
+      };
+    case UPDATE_PLAYER:
+      const newState = state.playersList.map(player => player.id === action.player.id ? action.player : player);
+      return {
+        ...state,
+        playersList: newState,
+      }
+    case UPDATE_PLAYER_PENDING:
+      return {
+        ...state,
+        pending: true,
+      };
+    case UPDATE_PLAYER_ERROR:
+      return {
+        ...state,
+        pending: false,
+        error: action.error,
+      };
+    case DELETE_PLAYER:
+      return {
+        ...state,
+        playersList: state.playersList.filter((data, i) => i !== action.player.id),
+      }
+    case DELETE_PLAYER_PENDING:
+      return {
+        ...state,
+        pending: true,
+      };
+    case DELETE_PLAYER_ERROR:
       return {
         ...state,
         pending: false,
